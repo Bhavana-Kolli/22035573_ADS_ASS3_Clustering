31126542dd320b5853ebd39b270b42b0a5517ead
Bhavana-Kolli <125909616+Bhavana-Kolli@users.noreply.github.com>
2023-05-09 19:06:39 +0100

    Add files via upload

---
+++ 22035573_ADS_Ass3.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+7PAM2000 Applied Data Science 1
+Assignment 3: Clustering and fitting
+
+@author: Bhavana Kolli - 22035573
+"""
+
+# Here modules are imported
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# from sklearn import cluster for clustering
+import sklearn.cluster as cluster
+import sklearn.metrics as skmet
+
+# to do fitting
+import scipy.optimize as opt
+
+# import modules provided
+import cluster_tools as ct
+import errors as err
+
+
+# Here functions are defined
+
