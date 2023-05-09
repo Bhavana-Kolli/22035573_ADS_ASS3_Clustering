4ad39a0681a180635f9cf246407a416b2a85f341
Bhavana-Kolli <125909616+Bhavana-Kolli@users.noreply.github.com>
2023-05-10 00:25:20 +0100

    Update 22035573_ADS_Ass3.py

--- 22035573_ADS_Ass3.py
+++ 22035573_ADS_Ass3.py
@@ -71,6 +71,67 @@ def read_df(filename):
     
     return df_years
 
+def get_year_data_merge(df1, df2, year1, year2):
+    """
+    Extracts a single year column from a given dataframe.
+    
+    Args:
+        df (pandas.DataFrame): The dataframe to extract data from.
+        year (int): The year to extract data for.
+    
+    Returns:
+        pandas.DataFrame: A dataframe containing the data for the given year.
+    """
+    
+    # drop rows with nan's in the given year
+    df1 = df1.dropna(subset=[year1, year2])
+    df2 = df2.dropna(subset=[year1, year2])
+    
+    df1_year = df1[["Country Name", year1, year2]].copy()
+    df2_year = df2[["Country Name", year1, year2]].copy()
+    
+    df_year = pd.merge(df1_year, df2_year, on="Country Name", how="outer")
+    
+    df_year = df_year.dropna() 
+    
+    return df_year
+
+
+def plot_correlation_heatmaps(df1, df2, title1, title2, size=6):
+    """
+    Plots correlation heatmaps side by side for two dataframes with 
+    two different scales.
+
+    Args:
+        df1, df2 (pandas DataFrame): Input dataframes
+        title1, title2 (str): Titles for the heatmaps
+        size (int): The vertical and horizontal size of the plot (in inches)
+    """
+    fig, axes = plt.subplots(1, 2, figsize=(2*size, size))
+
+    # Plot heatmap for df1 with coolwarm color scale
+    corr1 = df1.corr()
+    im1 = axes[0].matshow(corr1, cmap='coolwarm')
+    axes[0].set_xticks(range(len(corr1.columns)), fontsize=15)
+    axes[0].set_yticks(range(len(corr1.columns)), fontsize=15)
+    axes[0].set_xticklabels(corr1.columns, rotation=90, fontsize=15)
+    axes[0].set_yticklabels(corr1.columns, fontsize=15)
+    axes[0].set_title(title1, fontsize=15)
+    fig.colorbar(im1, ax=axes[0])
+
+    # Plot heatmap for df2 with coolwarm color scale
+    corr2 = df2.corr()
+    im2 = axes[1].matshow(corr2, cmap='coolwarm')
+    axes[1].set_xticks(range(len(corr2.columns)), fontsize=15)
+    axes[1].set_yticks(range(len(corr2.columns)), fontsize=15)
+    axes[1].set_xticklabels(corr2.columns, rotation=90, fontsize=15)
+    axes[1].set_yticklabels(corr2.columns, fontsize=15)
+    axes[1].set_title(title2, fontsize=15)
+    fig.colorbar(im2, ax=axes[1])
+
+    plt.tight_layout()
+    plt.show()
+
 
 # Main Program
 
@@ -94,3 +155,23 @@ print(df_co2.describe())
 # summary statistics for "GDP per capita (current US$)" of whole world
 print("\nGDP per capita summary statistics for whole world:")
 print(df_gdp.describe())
+
+df_co2_y = df_co2[[1990, 2000, 2010, 2019]]
+
+df_gdp_y = df_gdp[[1990, 2000, 2010, 2019]]
+
+# Plot correlation heatmaps for CO2 and GDP
+plot_correlation_heatmaps(df_co2_y, df_gdp_y, 
+                          "CO2 Emissions", "GDP per capita")
+
+# find data of co2 emissions & gdp per capita for the year 1990 and 2019
+df_1990_2019 = get_year_data_merge(df_co2, df_gdp, 1990, 2019)
+
+# rename columns
+df_1990_2019 = df_1990_2019.rename(columns={"1990_x":"co2 1990", 
+                                            "1990_y":"gdp 1990", 
+                                            "2019_x":"co2 2019", 
+                                            "2019_y":"gdp 2019"})
+print(df_1990_2019)
+print(df_1990_2019.describe())
+pd.plotting.scatter_matrix(df_1990_2019, figsize=(12, 12), s=5, alpha=0.8)
